6b7c34027ecc9d63b5a7abf1c96beadb19992c39
cwweng <cwweng@nuvoton.com>
2019-09-04 11:09:28 +0800

    Update nuc980_iot_defconfig and nuc980_iot.h
    The flash on NK-980IOT board is SPI NAND. Accessing SPI NAND should
    use nand command. Hence, enable NAND related configuration.
    
    Change-Id: Ia2f4e0a1a764590d0f8c3c074c91e2801aed8d5f

--- include/configs/nuc980_iot.h
+++ include/configs/nuc980_iot.h
@@ -45,7 +45,8 @@
 /*#define CONFIG_NUC980_HW_CHECKSUM */
 
 #define CONFIG_SYS_USE_SPIFLASH
-#define CONFIG_ENV_IS_IN_SPI_FLASH
+#define CONFIG_SYS_USE_NANDFLASH
+#define CONFIG_ENV_IS_IN_NAND
 
 #define CONFIG_BOARD_EARLY_INIT_F
 #define CONFIG_BOARD_LATE_INIT
@@ -92,7 +93,37 @@
 #undef CONFIG_CMD_JFFS2
 #endif
 
+#ifdef CONFIG_SYS_USE_NANDFLASH
+#define CONFIG_CMD_NAND		1
+#define CONFIG_CMD_UBI         1
+#define CONFIG_CMD_UBIFS       1
+#define CONFIG_MTD_UBI_WL_THRESHOLD  4096 /* CWWeng 2017.2.13 */
+#define CONFIG_MTD_UBI_BEB_LIMIT  20 /* CWWeng 2017.2.13 */
+#define CONFIG_CMD_MTDPARTS    1
+#define CONFIG_MTD_DEVICE      1
+#define CONFIG_MTD_PARTITIONS  1
+#define CONFIG_RBTREE          1
+#define CONFIG_LZO             1
+#define CONFIG_SYS_NAND_ONFI_DETECTION 1
+
+#define MTDIDS_DEFAULT "nand0=nand0"
+#define MTDPARTS_DEFAULT "mtdparts=nand0:0x200000@0x0(u-boot),0x1400000@0x200000(kernel),-(user)"
+#define MTD_ACTIVE_PART "nand0,2"
+
+/* #define CONFIG_CMD_NAND_YAFFS2 1 */
+/* #define CONFIG_YAFFS2          1 */
 #define CONFIG_SYS_MAX_NAND_DEVICE	1
+#define CONFIG_SYS_NAND_BASE	0xB000D000
+#define CONFIG_SYS_NAND_ECCBYTES        12
+#ifdef CONFIG_ENV_IS_IN_NAND
+#define CONFIG_ENV_OFFSET       0x80000
+#define CONFIG_ENV_SIZE         0x20000
+#define CONFIG_ENV_SECT_SIZE    0x20000
+#define CONFIG_ENV_RANGE	        (4 * CONFIG_ENV_SECT_SIZE)  /* Env range : 0x80000 ~ 0x100000 */
+#define CONFIG_ENV_OVERWRITE
+#endif
+#endif
+
 #define CONFIG_SYS_NAND_U_BOOT_OFFS     0x100000   /* Offset to RAM U-Boot image */
 
 #define CONFIG_SPL_TEXT_BASE	0x200
